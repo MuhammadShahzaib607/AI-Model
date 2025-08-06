91b76eb31dcfac333ba82d50a7ae3d4dba31cdd8
MuhammadShahzaib607 <muhammadshahzaib6077@gmail.com>
2025-08-06 11:55:36 -0700

    URL change

--- client/src/components/AiModel/AiModel.jsx
+++ client/src/components/AiModel/AiModel.jsx
@@ -37,7 +37,7 @@ const AiModel = () => {
     setLoading(true)
 
     try {
-      const res = await axios.post("http://localhost:8000/", {
+      const res = await axios.post("https://ai-model-orcin-one.vercel.app/", {
         prompt: userInput
       })
 
